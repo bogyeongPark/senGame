b2e2d6f4c3a7a2fd1b2a680a4e21a93be993cfc2
bogyeongPark <parkcsr0914@gmail.com>
2021-05-07 02:35:32 +0900

    문장 버튼화 완료

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
-
+from flask import Flask, render_template,url_for,request,jsonify
+from pororo import Pororo
+import random
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello this is the new version!"
+    return render_template('index.html')
+
+@app.route("/study",methods=['POST'])
+def study():
+    # t = 公司安排我下午出差。
+    # a = '회사는 내가 오후에 출장 가는 것으로  처리했다.'
+    if request.method=='POST':
+        lableSen = request.form['kosen']
+        sen = request.form['zhsen']
+        targetSen = sen.strip()
+        print(targetSen)
+        tk = Pororo(task='tokenize',lang='zh')
+        tokens = tk(targetSen)
+        print(tokens)
+        random.shuffle(tokens)
+        jsonify(tokens)
+    return render_template('studymode.html',value=tokens,translation=lableSen)
+
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',debug=True)
